51c5eeaa551da8bea3d40a464ac9f59f80833e90
ああああ <toypoodlelisatoyoda@gmail.com>
2020-09-03 18:34:41 +0900

    Initial Commit

---
+++ cinderella note/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cinderella note
+//
+//  Created by 豊田莉彩 on 2020/09/03.
+//  Copyright © 2020 豊田莉彩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
